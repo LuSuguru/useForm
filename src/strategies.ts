af54d1cd8bee69407929f301a88cfc788a22a1f4
LuSuguru <429150413@qq.com>
2019-11-19 15:21:05 +0800

    fix: 修复校验问题

--- src/strategies.ts
+++ src/strategies.ts
@@ -8,43 +8,43 @@ function isRequired({ value, errorMsg }) {
 }
 
 function isPrice({ value, errorMsg }) {
-  if (value && !value.match(/^(0|[1-9]+)(\.[0-9]+)?$/)) {
+  if (value && !value.toString().match(/^(0|[1-9]+)(\.[0-9]+)?$/)) {
     return errorMsg
   }
 }
 
 function isMobile({ value = '', errorMsg }) {
-  if (value && !value.match(/^(0|86|17951)?(13[0-9]|15[0-9]|17[0-9]|18[0-9]|14[0-9])[0-9]{8}$/)) {
+  if (value && !value.toString().match(/^(0|86|17951)?(13[0-9]|15[0-9]|17[0-9]|18[0-9]|14[0-9])[0-9]{8}$/)) {
     return errorMsg
   }
 }
 
 function isIdentity({ value, errorMsg }) {
-  if (value && !value.match(/^\d{17}[0-9Xx]$/)) {
+  if (value && !value.toString().match(/^\d{17}[0-9Xx]$/)) {
     return errorMsg
   }
 }
 
 function isReg({ value, pattern, errorMsg }) {
-  if (value && !value.match(pattern)) {
+  if (value && !value.toString().match(pattern)) {
     return errorMsg
   }
 }
 
 function isVerify({ value, errorMsg }) {
-  if (value && !value.match(/\d{4}/)) {
+  if (value && !value.toString().match(/\d{4}/)) {
     return errorMsg
   }
 }
 
 function isMax({ value, max, errorMsg }) {
-  if (value && value.length > max) {
+  if (value && value.toString().length > max) {
     return errorMsg
   }
 }
 
 function isMin({ value, min, errorMsg }) {
-  if (value && value.length < min) {
+  if (value && value.toString().length < min) {
     return errorMsg
   }
 }
