56bc434e2663016827342222f62514c8207f9034
LuSuguru <429150413@qq.com>
2020-05-06 13:09:23 +0800

    feat: 修改onChange 逻辑

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ export default function <T>(): UseForm<T> {
 
     formMemoInfo.current[formName] = true
 
-    const { valuePropName = 'value', initialValue, rules, autoValidator = true, normalize, getValueformEvent } = options || defaultOptions
+    const { valuePropName = 'value', initialValue, rules, autoValidator = true, normalize, getValueformEvent, onChange } = options || defaultOptions
 
     formDefs.current[formName] = options || defaultOptions
 
@@ -106,8 +106,8 @@ export default function <T>(): UseForm<T> {
         setFormData({ [formName]: newValue })
 
         // 调用 options 里的 onChange
-        if (options.onChange) {
-          options.onChange(newValue)
+        if (onChange) {
+          onChange(newValue)
         }
       },
     }
